ea35cac8d68d56fa1c08efc7676e838333ceae7f
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-04-14 13:10:01 +0900

    画像の２値化

---
+++ chapters/opencv/threshold.py
@@ -0,0 +1,33 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+def main():
+    threshold = 125
+
+    # 画像の読み込み
+    img = cv2.imread("images/ramen_man.jpg")
+
+    # 画像のグレースケール化
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 画像の二値化(閾値 125 を超えた画素を255にする。)
+    ret, img_thresh = cv2.threshold(img_gray, threshold, 255, cv2.THRESH_BINARY)
+
+    # 画像の保存
+    cv2.imwrite("images/ramen_man_thresh.jpg", img_thresh)
+
+    # 画像の表示
+    plt.subplot(121)
+    plt.title("Original")
+    plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    plt.axis("off")
+    plt.subplot(122)
+    plt.title("Threshold")
+    plt.imshow(cv2.cvtColor(img_thresh, cv2.COLOR_BGR2RGB))
+    plt.axis("off")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
