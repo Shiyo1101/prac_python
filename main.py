454ac4ecfa5034ebb3ca2bea1845e8eea5e305f0
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-04-14 11:53:32 +0900

    画像変換テスト

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+import matplotlib.pyplot as plt
+
+def main():
+    #png to jpg
+    img = cv2.imread("test.png")
+    cv2.imwrite("test.jpg", img)
+
+    jpgImg = cv2.imread("test.jpg")
+    plt.imshow(jpgImg)
+
+    plt.show()
+
+
+
+
+if __name__ == "__main__":
+    main()
