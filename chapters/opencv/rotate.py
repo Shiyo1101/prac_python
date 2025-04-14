7bd937e15ec48a9dae97b0f63a10041ef9bf9160
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-04-14 12:47:12 +0900

    画像の回転

---
+++ chapters/opencv/rotate.py
@@ -0,0 +1,22 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+def main():
+    # 画像の読み込み
+    img = cv2.imread("images/test.png")
+
+    # 画像の回転
+    img_rotate = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+
+    # 画像の保存
+    cv2.imwrite("images/test_rotate.png", img_rotate)
+
+    # 画像の表示
+    plt.imshow(cv2.cvtColor(img_rotate, cv2.COLOR_BGR2RGB))
+    plt.axis("off")
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
