fd8561991090549c8c9687b6b150f9a8e573e900
Shiyo1101 <yoshiki1011abc@gmail.com>
2025-04-14 12:06:08 +0900

    リファクタ

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import cv2
-import matplotlib.pyplot as plt
-
-
 def main():
-    # png to jpg
-    img = cv2.imread("test.png")
-    cv2.imwrite("test.jpg", img)
-
-    jpgImg = cv2.imread("test.jpg")
-    plt.imshow(jpgImg)
-
-    plt.show()
+    print("hello world")
 
 
 if __name__ == "__main__":
